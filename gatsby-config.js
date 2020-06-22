2dd7e17c5e9d83f21467c8bd3e5173bd2fb4e82b
George Adams <georgeadams1995@gmail.com>
2020-06-22 10:36:27 +0100

    add google analytics (#178)

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-99905649-2",
+        anonymize: true,
       },
     },
     {
