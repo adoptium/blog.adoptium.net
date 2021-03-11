8f6b12e18a710039961f3a713c15755a2409ff78
George Adams <georgeadams1995@gmail.com>
2021-03-11 09:34:07 +0000

    final adoptopenjdk cleanup
    Signed-off-by: George Adams <georgeadams1995@gmail.com>

--- src/components/guestpost.js
+++ src/components/guestpost.js
@@ -3,7 +3,7 @@ import React from "react";
 const GuestPost = ({ children }) => {
   return (
     <p className="guestpost">
-      <em>{children} – AdoptOpenJDK Team</em>
+      <em>{children} – Adoptium Team</em>
     </p>
   );
 };
